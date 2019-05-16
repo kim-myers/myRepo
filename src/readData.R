227be125eaab1285ef6ff9ae12d34d788d3b5645
kim-myers <50703114+kim-myers@users.noreply.github.com>
2019-05-16 14:47:03 -0400

    Update readData.R

--- src/readData.R
+++ src/readData.R
@@ -19,3 +19,5 @@ gm182UP <- read_csv(here::here('data', '2018-11-27_Gm182-UserPoints-Start-CEE-Lo
 # Read in Gm182 Data: 100 estimated positions of Gm182
 gm182 <- read_csv(here::here('data', '2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv')) %>% 
   mutate(status = 'noUserPoints')
+
+##ciao
